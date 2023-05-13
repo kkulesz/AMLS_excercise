da43907f7f5b42ccb70aa1f04704d0eba6bb1c3c
kkulesz <kulesza.konrad@protonmail.com>
2023-05-13 14:19:16 +0200

    add downloading cords files in download_data.py script

--- data_acquistion_and_alignment/download_data.py
+++ data_acquistion_and_alignment/download_data.py
@@ -4,6 +4,7 @@ import bz2
 import logging
 
 BASE_URL = "https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301"
+BASE_CORDS_URL = "https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj"
 DATA_DIRECTORY = "../data/"
 LOG_FILE = "log.txt"
 
@@ -19,11 +20,15 @@ logging.info("Running Urban Planning")
 logger = logging.getLogger('urbanGUI')
 
 
-def build_url(run_num, filter_t, camera_col, sequence_num, f_ext):
+def build_data_url(run_num, filter_t, camera_col, sequence_num, f_ext):
     return f"{BASE_URL}/{run_num}/{camera_col}/frame-{filter_t}-{str(run_num).zfill(6)}-{camera_col}-{str(sequence_num).zfill(4)}.{f_ext}"
 
 
-def download_or_log_at_fail(url, dir):
+def build_cords_url(run_num, camera_col, cords_of_what):
+    return f"{BASE_CORDS_URL}-{str(run_num).zfill(6)}-{camera_col}-{cords_of_what}.fits.gz"
+
+
+def download_or_log_on_fail(url, dir):
     try:
         wget.download(url, dir)
     except:
@@ -54,11 +59,20 @@ if __name__ == "__main__":
 
     for (run_num, min_seq, max_seq) in urls_meta_numbers:
         for camera_col in range(1, 6):
-            for seq_num in range(min_seq, max_seq):
-                for band in ["g", "i", "r", "u", "z"]:
-                    url = build_url(run_num, band, camera_col, seq_num, "fits.bz2")
-                    download_or_log_at_fail(url, DATA_DIRECTORY)
 
-                for img_type in ["irg", "thumb-irg"]:
-                    url = build_url(run_num, img_type, camera_col, seq_num, "jpg")
-                    download_or_log_at_fail(url, DATA_DIRECTORY)
+            # download coordinate files
+            for cords_of_what in ["gal", "star"]:  # 2 * ~35MB = ~~70MB
+                url = build_cords_url(run_num, camera_col, cords_of_what)
+                download_or_log_on_fail(url, DATA_DIRECTORY)
+            print(f"Cords downloaded: {run_num}:{camera_col}")
+
+            for seq_num in range(min_seq, max_seq):
+                # download various bands of single image
+                for band in ["g", "i", "r", "u", "z"]:  # 5 * ~3.5MB = ~~18MB
+                    url = build_data_url(run_num, band, camera_col, seq_num, "fits.bz2")
+                    download_or_log_on_fail(url, DATA_DIRECTORY)
+
+                # download irg jpgs
+                for img_type in ["irg", "thumb-irg"]:  # less than 1MB combined, negligible,
+                    url = build_data_url(run_num, img_type, camera_col, seq_num, "jpg")
+                    download_or_log_on_fail(url, DATA_DIRECTORY)
