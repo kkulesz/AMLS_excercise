621db9722777d0d4e9b93ae00e1c880f72a066c8
kkulesz <kulesza.konrad@protonmail.com>
2023-06-15 15:55:57 +0200

    debug

--- data_preparation/split_into_smaller_pieces.py
+++ data_preparation/split_into_smaller_pieces.py
@@ -14,7 +14,6 @@ def get_multiple_without_reminder(N, divider):
 def split_into_smaller_pieces(files, is_target, directory):
     for f in files:
         img = np.load(f)
-
         H, W, _ = img.shape
         nH, nW = const.PIECE_SHAPE
 
@@ -28,6 +27,7 @@ def split_into_smaller_pieces(files, is_target, directory):
             path = os.path.join(directory, piece_file_name)
             np.save(path, piece, allow_pickle=True)
 
+
 if __name__ == "__main__":
     utils.create_dir_if_doesnt_exist(const.PIECES_READY_DATA_DIR)
     utils.create_dir_if_doesnt_exist(const.PIECES_READY_DATA_INPUTS_DIR)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     input_files = utils.listdir_fullpath(const.ALIGNED_DATA_DIR)
     target_files = utils.listdir_fullpath(const.TARGET_DATA_DIR)
-    target_files = list(filter(lambda f: "target" in f, target_files))
+    target_files = list(filter(lambda f: "_target" in f, target_files))
 
     split_into_smaller_pieces(input_files, is_target=False, directory=const.PIECES_READY_DATA_INPUTS_DIR)
     split_into_smaller_pieces(target_files, is_target=True, directory=const.PIECES_READY_DATA_TARGETS_DIR)
