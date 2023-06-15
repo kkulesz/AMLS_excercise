af8067f37efbd01e529e63b02ceabc67611e7ffb
kkulesz <kulesza.konrad@protonmail.com>
2023-06-15 16:46:21 +0200

    make UnetV3 smaller

--- modeling_and_tuning/models/unet_v3.py
+++ modeling_and_tuning/models/unet_v3.py
@@ -53,11 +53,11 @@ class UnetV3(nn.Module):
         self.e1 = EncoderBlock(5, 64)
         self.e2 = EncoderBlock(64, 128)
         self.e3 = EncoderBlock(128, 256)
-        self.e4 = EncoderBlock(256, 512)
+        # self.e4 = EncoderBlock(256, 512)
         """ Bottleneck """
-        self.b = ConvBlock(512, 1024)
+        self.b = ConvBlock(256, 512)
         """ Decoder """
-        self.d1 = DecoderBlock(1024, 512)
+        # self.d1 = DecoderBlock(1024, 512)
         self.d2 = DecoderBlock(512, 256)
         self.d3 = DecoderBlock(256, 128)
         self.d4 = DecoderBlock(128, 64)
@@ -69,12 +69,12 @@ class UnetV3(nn.Module):
         s1, p1 = self.e1(inputs)
         s2, p2 = self.e2(p1)
         s3, p3 = self.e3(p2)
-        s4, p4 = self.e4(p3)
+        # s4, p4 = self.e4(p3)
         """ Bottleneck """
-        b = self.b(p4)
+        b = self.b(p3)
         """ Decoder """
-        d1 = self.d1(b, s4)
-        d2 = self.d2(d1, s3)
+        # d1 = self.d1(b, s4)
+        d2 = self.d2(b, s3)
         d3 = self.d3(d2, s2)
         d4 = self.d4(d3, s1)
         """ Classifier """
