030aeeffa88561c6686da06b45ff4b529bf07d0d
kkulesz <kulesza.konrad@protonmail.com>
2023-06-20 18:33:23 +0200

    add WeightsAndBiases, try DiceLoss

--- modeling_and_tuning/train.py
+++ modeling_and_tuning/train.py
@@ -6,6 +6,7 @@ import wandb
 
 from data_preparation.dataset import SdssDataset
 from models.unet import UNet
+from criterions.dice_loss import DiceLoss
 import utils
 import const
 
@@ -20,24 +21,32 @@ def train_single_epoch(model, loader, optimizer, criterion):
 
         loss = criterion(prediction_tensor, target_tensor)
 
+        wandb.log(
+            {"loss": loss}
+        )
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
 
 if __name__ == "__main__":
-    model_name = "model-1000epochs-without-validate.pt"
+    wandb.login()
+    wandb.init(project="AMLS", entity="luizz")
+
+    model_name = "model.pt"
     mo = UNet(in_channels=const.INPUT_CHANNELS, out_channels=const.OUTPUT_CHANNELS, bilinear=const.BILINEAR).to(device)
 
     dataset = SdssDataset(const.PIECES_READY_DATA_INPUTS_DIR, const.PIECES_READY_DATA_TARGETS_DIR)
     dataloader = DataLoader(dataset, batch_size=const.BATCH_SIZE, shuffle=True)
     opt = Adam(mo.parameters(), lr=const.LEARNING_RATE, betas=const.ADAM_BETAS)
-    crt = nn.CrossEntropyLoss() if const.OUTPUT_CHANNELS > 1 else nn.BCEWithLogitsLoss()
+
+    # crt = nn.CrossEntropyLoss() if const.OUTPUT_CHANNELS > 1 else nn.BCEWithLogitsLoss()
+    crt = DiceLoss()
 
     for epoch in range(const.NUMBER_OF_EPOCHS):
-        print(f"epoch={epoch+1}")
+        print(f"epoch={epoch + 1}")
         train_single_epoch(mo, dataloader, opt, crt)
-        if (epoch + 1) % 50 == 0:
+        if (epoch + 1) % const.SAVE_MODEL_INTERVAL == 0:
             print("Saving model...")
-            torch.save(mo.state_dict(), "../models_storage/model-150epochs-without.pt")
+            torch.save(mo.state_dict(), f"{model_name}-checkpoint-epoch={epoch+1}")
     torch.save(mo.state_dict(), model_name)
